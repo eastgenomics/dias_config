c6713db325f1ea415a8d101bae1594bad61abdff
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-08 14:53:40 +0000

    update g2t to 201208

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -2,4 +2,4 @@ project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
 genepanels_file = "{}:file-Fq3yY48433GxY9VQ9ZZ9ZfqX".format(project_id)
 bioinformatic_manifest = "{}:file-FvyX44j433Gz74z60Vg0QgkG".format(project_id)
-nirvana_genes2transcripts = "{}:file-Fyq67z8433GZGxZJ3z3jK92Z".format(project_id)
+nirvana_genes2transcripts = "{}:file-Fz7k1KQ433GVq1v45V2j989V".format(project_id)
