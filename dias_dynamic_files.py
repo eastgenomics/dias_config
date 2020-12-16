79213875e2a6f62e2d83e623fc29d2e20b899da3
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-16 12:38:39 +0000

    update genepanels to 201216, biomanifest to 201216

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -1,5 +1,5 @@
 project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
-genepanels_file = "{}:file-Fq3yY48433GxY9VQ9ZZ9ZfqX".format(project_id)
-bioinformatic_manifest = "{}:file-FvyX44j433Gz74z60Vg0QgkG".format(project_id)
+genepanels_file = "{}:file-FzGzZ4j433GQ911kP8GkvqK1".format(project_id)
+bioinformatic_manifest = "{}:file-FzGzYvj433GpgbVF8q6yqvyP".format(project_id)
 nirvana_genes2transcripts = "{}:file-Fz7k1KQ433GVq1v45V2j989V".format(project_id)
