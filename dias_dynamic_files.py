6b1934a542b30e5a0fb8934a1915201391d68f6a
Yu-jinKim <yujin.kim@hotmail.fr>
2021-03-12 11:38:56 +0000

    point to g2t 210312

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -2,4 +2,4 @@ project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
 genepanels_file = "{}:file-FzGzZ4j433GQ911kP8GkvqK1".format(project_id)
 bioinformatic_manifest = "{}:file-FzGzYvj433GpgbVF8q6yqvyP".format(project_id)
-nirvana_genes2transcripts = "{}:file-FzKKQ4j433GXy8J5J662jZJQ".format(project_id)
+nirvana_genes2transcripts = "{}:file-G15b2VQ433Gqx8fq1yj7jFx7".format(project_id)
