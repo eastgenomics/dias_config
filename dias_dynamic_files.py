e9b41dd6b47ee00ca03d0120c9e7d241d24cba6d
Yu-jinKim <yujin.kim@hotmail.fr>
2021-04-30 15:12:25 +0100

    missing colon...

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -2,4 +2,4 @@ project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
 genepanels_file = "{}:file-G1QvZP0433GQqbbY6951JvGf".format(project_id)
 bioinformatic_manifest = "{}:file-G1QvZbQ433GYX70v8f0YBxgb".format(project_id)
-nirvana_genes2transcripts = "{}file-G1qQ9JQ433GZ4G8Q5Z1xy0ZF".format(project_id)
+nirvana_genes2transcripts = "{}:file-G1qQ9JQ433GZ4G8Q5Z1xy0ZF".format(project_id)
