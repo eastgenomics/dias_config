6541265ee70ddccc081108da1b9859cd899d3587
Yu-jinKim <yujin.kim@hotmail.fr>
2021-05-27 14:03:41 +0100

    g2t with hgnc:11013

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -5,4 +5,4 @@ genepanels_file = "{}:file-G2gg1F0433GjQbzVKgYpQfX0".format(project_id)
 # manifest 210526
 bioinformatic_manifest = "{}:file-G2gg12Q433Gp47b0JGGzZXZB".format(project_id)
 # g2t 210526
-nirvana_genes2transcripts = "{}:file-G2gg1Qj433GZx8j686G7XY9x".format(project_id)
+nirvana_genes2transcripts = "{}:file-G2gjxVj433GqpFB3K45bZx6V".format(project_id)
