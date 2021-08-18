f1506d8862803d9eaefe8d74f52a0258b10bf08f
Yujin Kim <yujin.kim@hotmail.fr>
2021-08-18 11:28:29 +0100

    change g2t 210722 to 210813

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -5,4 +5,4 @@ genepanels_file = "{}:file-G3x80g0433GxJkq36yYG6441".format(project_id)
 # manifest 210526
 bioinformatic_manifest = "{}:file-G3x7zb8433GzPJ1y534XV8VP".format(project_id)
 # g2t 210526
-nirvana_genes2transcripts = "{}:file-G3x7y98433GzPJ1y534XV8QB".format(project_id)
+nirvana_genes2transcripts = "{}:file-G4J3zxQ433GxFpKQBbvJ1J7g".format(project_id)
