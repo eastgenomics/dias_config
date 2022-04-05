ac238d354ef42ab2234491650d07a5c79f74d05c
Yujin Kim <yujin.kim@hotmail.fr>
2022-04-05 11:25:07 +0100

    update comments 220330

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -1,8 +1,8 @@
 project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
-# genepanels 211019
+# genepanels 220330
 genepanels_file = "{}:file-G961GFj433Gv5X7J14pGKBy8".format(project_id)
-# manifest 211019
+# manifest 220330
 bioinformatic_manifest = "{}:file-G961GPQ433GXkvqv2q47fy3f".format(project_id)
 # g2t 211112
 nirvana_genes2transcripts = "{}:file-G6F2P8j433GQXQ8K6kg22jK5".format(project_id)
