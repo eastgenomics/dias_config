3f30305089938a7f4fe9b0f940af43f2f98fd5e1
Yujin Kim <yujin.kim@hotmail.fr>
2022-11-01 10:09:44 +0000

    221027 manifest

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -2,7 +2,7 @@ project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
 # genepanels 220628
 genepanels_file = "{}:file-GBxP5Z8433GfKq081gj59q82".format(project_id)
-# manifest 220810
-bioinformatic_manifest = "{}:file-GFkv5gQ433GxjPzB0Zp1QYg3".format(project_id)
+# manifest 221027
+bioinformatic_manifest = "{}:file-GJJ7xz0433GyJ7X2B4ybbFB1".format(project_id)
 # g2t 220609
 genes2transcripts = "{}:file-GF613kj433Gy5P4v5FZqPk3j".format(project_id)
