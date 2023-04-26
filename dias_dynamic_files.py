6fb99ea3ea3430340c38994723ec31c2c15a9263
Yujin Kim <yujin.kim@hotmail.fr>
2023-04-26 11:40:17 +0100

    update genepanels and g2t to integrate April 2023 TD update

--- dias_dynamic_files.py
+++ dias_dynamic_files.py
@@ -1,8 +1,8 @@
 project_id = "project-Fkb6Gkj433GVVvj73J7x8KbV"
 
-# genepanels 221027
-genepanels_file = "{}:file-GJJ7Vx8433Gz96yp8V98X74f".format(project_id)
+# genepanels 230421
+genepanels_file = "{}:file-GV4PPy8433GZ9bX8kKVkfbX5".format(project_id)
 # manifest 221027
 bioinformatic_manifest = "{}:file-GJJ7xz0433GyJ7X2B4ybbFB1".format(project_id)
-# g2t 230123
-genes2transcripts = "{}:file-GP7FY50433GZX7x0JqfgBB4q".format(project_id)
+# g2t 230421
+genes2transcripts = "{}:file-GV4P970433Gj6812zGVBZvB4".format(project_id)
